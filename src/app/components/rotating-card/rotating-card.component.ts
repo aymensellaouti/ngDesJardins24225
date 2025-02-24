242d6dc71ab4f02abf931fd97c93ea375ca52ccc
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-24 20:59:32 +0100

    correction rotating Card

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rotating-card',
   templateUrl: './rotating-card.component.html',
-  styleUrls: ['./rotating-card.component.css']
+  styleUrls: ['./rotating-card.component.css'],
 })
 export class RotatingCardComponent {
-
+  // On doit définir le state (ce qu'on veut manipuler dans le HTML)
+  name = 'Sellaouti';
+  firstname = 'Aymen';
+  job = 'Teacher';
+  path = 'rotating_card_profile3.png';
+  // On les affiche
+  // onles bind en two way avec les inputs
 }
