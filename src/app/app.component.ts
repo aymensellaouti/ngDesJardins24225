b8472846a8e33075cb2bc875526bec9fea1be035
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-24 17:04:13 +0100

    mise en place de bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngDesJardins24225';
-}
+export class AppComponent {}
