3a96d3b621a55add2b016bd333222ac6a4ed83a3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-24 18:01:44 +0100

    installation de nos premiers composants

---
+++ src/app/components/second/second.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `
+    <p>Second fonctionne</p>
+  `,
+  styles: [``]
+})
+export class SecondComponent {
+
+}
