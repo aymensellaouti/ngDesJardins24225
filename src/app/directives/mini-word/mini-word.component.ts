1360e2260c9b41e5a05f937a0ad1af4fff253baa
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-25 16:10:57 +0100

    exercice mini word

--- src/app/directives/mini-word/mini-word.component.ts
+++ src/app/directives/mini-word/mini-word.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./mini-word.component.css']
 })
 export class MiniWordComponent {
-
+  color = 'black';
+  size = 75;
+  fontFamily = 'garamond';
 }
